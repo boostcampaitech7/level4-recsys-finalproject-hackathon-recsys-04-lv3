84c1addd90d862d8fbad01f64801dce9bdef80ef
SooMiiii <smlee010303@gmail.com>
2025-02-05 11:55:37 +0900

    Refactor: Modify Python files to match DB changes
    [Setting] DB 최종 설정 #61

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,9 +1,7 @@
-from app.models.analysis import Analysis
-from app.models.code import Code
-from app.models.multiple import MultipleChoice
-from app.models.note import Note
 from app.models.ox import OX
-from app.models.rag import RAG
+from app.models.note import Note
 from app.models.user import User
+from app.models.analysis import Analysis
+from app.models.multiple import MultipleChoice
 
-__all__ = ["User", "Note", "OX", "Code", "Analysis", "RAG", "MultipleChoice"]
+__all__ = ["User", "Note", "OX", "Analysis", "MultipleChoice"]
--- backend/app/models/analysis.py
+++ backend/app/models/analysis.py
@@ -8,5 +8,5 @@ class Analysis(TimeStampedBase):
     analyze_id = Column(String(10), primary_key=True, comment='분석 고유 아이디')
     note_id = Column(String(10), ForeignKey("tb_note.note_id"), nullable=True, comment='노트 아이디')
     chunk_num = Column(Integer, nullable=True, comment='청크 번호')
-    rag_id = Column(String(100), ForeignKey("tb_rag.rag_id"), nullable=True, comment='RAG 아이디')
+    rag_id = Column(String(300), nullable=True, comment='RAG 아이디')
     feedback = Column(Text, nullable=True, comment='분석 피드백')
